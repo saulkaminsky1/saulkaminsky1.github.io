8d5ada71681902fc305c9a21963e5120ad579347
saulkaminsky <39011342+saulkaminsky@users.noreply.github.com>
2020-05-01 13:27:34 -0300

    commit

---
+++ js/teste.js
@@ -0,0 +1,9 @@
+//Função 2. Busco Experiência
+const experienciaEstagio = function(experienciasPretendidas) {
+    if (experienciasPretendidas >= 1) {
+        console.log("Esse estágio vai valer a pena !! ")
+    } else{
+        console.log('Melhor buscar outro estágio')
+        }
+}
+experienciaEstagio(1)
